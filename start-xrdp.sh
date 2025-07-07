193f2a4942e35f3f90040c73ba5d05667709729c
Simon Li <spli@dundee.ac.uk>
2025-07-07 11:46:47 +0100

    Default to requiring NEW_PASSWORD for XRDP

--- start-xrdp.sh
+++ start-xrdp.sh
@@ -8,12 +8,20 @@ if [ ! -f "$XRDP_DIR/key.pem" -o ! -f "$XRDP_DIR/cert.pem" ]; then
 fi
 
 if [ ! -f "$XRDP_DIR/xrdp.ini" ]; then
-  if [ -z "${NEW_PASSWORD:-}" ]; then
-    export NEW_PASSWORD=$(openssl rand -base64 33)
+  if [ "${XRDP_NO_PASSWORD:-}" = "1" ]; then
+    if [ -z "${NEW_PASSWORD:-}" ]; then
+      export NEW_PASSWORD=$(openssl rand -base64 33)
+    fi
+    /etc/xrdp/passwd.expect
+    USERNAME=$(id -un)
+    sed -e "s|username=ask|username=$USERNAME|" -e "s|password=ask|password=$NEW_PASSWORD|" /etc/xrdp/xrdp.ini > "$XRDP_DIR/xrdp.ini"
+  elif [ -z "${NEW_PASSWORD:-}" ]; then
+    echo "NEW_PASSWORD must be set unless XRDP_NO_PASSWORD=1"
+    exit 1
+  else
+    /etc/xrdp/passwd.expect
+    cp /etc/xrdp/xrdp.ini "$XRDP_DIR/xrdp.ini"
   fi
-  /etc/xrdp/passwd.expect
-  USERNAME=$(id -un)
-  sed -e "s|username=ask|username=$USERNAME|" -e "s|password=ask|password=$NEW_PASSWORD|" /etc/xrdp/xrdp.ini > "$XRDP_DIR/xrdp.ini"
 fi
 
 if [ ! -d /home/ubuntu/Desktop ]; then
